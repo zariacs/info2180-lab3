f59f1003364a438c730745abced32b68040bfca4
Zaria Chen Shui <zariachenshui@gmail.com>
2022-10-08 22:08:23 -0500

    Updated tic-tac-toe.js to add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,34 @@
 window.onload = function () {
     addSquareClassToSquares();
+    addClickListenersToSquares();
 }
 
 function addSquareClassToSquares() {
     const allSquares = document.getElementById("board").children;
     for (var square of allSquares) {
         square.classList.add("square");
+    }
+}
+
+function addClickListenersToSquares() {
+    const allSquares = document.getElementsByClassName("square");
+    for (var square of allSquares) {
+        // square.onclick = addXO(square);
+        square.addEventListener("click", addXO);
+    }
+}
+
+let moves = [];
+
+function addXO() {
+    if (moves.length % 2 == 0) {
+        this.className = "square X";
+        this.innerHTML = "X";
+        moves.push("x");
+    } else {
+        this.className = "square O";
+        this.innerHTML = "O";
+        moves.push("o");
     }
 }
 
